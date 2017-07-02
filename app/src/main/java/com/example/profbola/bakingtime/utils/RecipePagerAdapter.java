77aafc4e22a8e72761362e1d2106230c657c79cf
Ehbraheem <ehbraheem@gmail.com>
2017-07-02 16:16:28 -0700

    Add RecipePagerAdapter for ViewPager TabLayout

---
+++ app/src/main/java/com/example/profbola/bakingtime/utils/RecipePagerAdapter.java
@@ -0,0 +1,43 @@
+package com.example.profbola.bakingtime.utils;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by prof.BOLA on 7/2/2017.
+ */
+
+public class RecipePagerAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    public RecipePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+        notifyDataSetChanged();
+    }
+}
