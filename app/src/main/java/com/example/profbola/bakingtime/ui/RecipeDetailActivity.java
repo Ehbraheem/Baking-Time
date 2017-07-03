594e982ee2072f0249dd2820399b70e68c78cee6
Ehbraheem <ehbraheem@gmail.com>
2017-07-03 05:54:42 -0700

    Implement OnVideoPlayerSelected and OnIngredientSelected

--- app/src/main/java/com/example/profbola/bakingtime/ui/RecipeDetailActivity.java
+++ app/src/main/java/com/example/profbola/bakingtime/ui/RecipeDetailActivity.java
@@ -1,6 +1,8 @@
 package com.example.profbola.bakingtime.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -8,14 +10,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.example.profbola.bakingtime.R;
+import com.example.profbola.bakingtime.models.Ingredient;
 import com.example.profbola.bakingtime.models.Recipe;
+import com.example.profbola.bakingtime.models.Step;
 import com.example.profbola.bakingtime.utils.RecipePagerAdapter;
 
 /**
  * Created by prof.BOLA on 7/1/2017.
  */
 
-public class RecipeDetailActivity extends AppCompatActivity {
+public class RecipeDetailActivity extends AppCompatActivity implements
+        IngredientsFragment.OnIngredientSelected, StepsFragment.OnVideoPlayerSelected {
 
     private static final String INGREDIENT_TAB = "ingredients";
     private static final String STEP_TAB = "steps";
@@ -26,11 +31,29 @@ public class RecipeDetailActivity extends AppCompatActivity {
 
     private IngredientsFragment mIngredientsFragment;
     private StepsFragment mStepsFragment;
+    private FullDetailsFragment mFullDetailsFragment;
+
+    private boolean mTwoPane;
+
+    public static final String INGREDIENTS_KEY = "ingredient";
+    public static final String STEP_KEY = "step";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recipe_detail_activity);
+
+        if (findViewById(R.id.recipe_full_view) != null) {
+            mTwoPane = true;
+
+            mFullDetailsFragment = new FullDetailsFragment();
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.recipe_full_view, mFullDetailsFragment)
+                    .commit();
+        } else {
+            mTwoPane = false;
+        }
 
         if (savedInstanceState == null) {
             Recipe recipe = (Recipe) getIntent().getExtras().get("NAME");
@@ -64,4 +87,29 @@ public class RecipeDetailActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
     }
 
+    @Override
+    public void ingredientClicked(Ingredient ingredient) {
+        routeToFragmentOrActivity(ingredient, INGREDIENTS_KEY);
+    }
+
+    @Override
+    public void videoClicked(Step step) {
+        routeToFragmentOrActivity(step, STEP_KEY);
+    }
+
+    private void routeToFragmentOrActivity(Parcelable object, String type) {
+        if (mTwoPane) {
+            FullDetailsFragment newFragment = new FullDetailsFragment();
+            Bundle bundle = new Bundle();
+            bundle.putParcelable(type, object);
+            newFragment.setArguments(bundle);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.recipe_full_view, newFragment)
+                    .commit();
+        } else {
+            Intent intent = new Intent(this, FullDetailActivity.class);
+            intent.putExtra(type, object);
+            startActivity(intent);
+        }
+    }
 }
