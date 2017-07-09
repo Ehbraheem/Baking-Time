313275c45be1080a66934d3ea05488e5bbef29fc
Ehbraheem <ehbraheem@gmail.com>
2017-07-09 01:01:05 -0700

    Add IsPersitable interface

--- app/src/main/java/com/example/profbola/bakingtime/utils/RecipeUtils.java
+++ app/src/main/java/com/example/profbola/bakingtime/utils/RecipeUtils.java
@@ -1,6 +1,12 @@
 package com.example.profbola.bakingtime.utils;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.os.Parcelable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by prof.BOLA on 7/6/2017.
@@ -14,5 +20,11 @@ public class RecipeUtils {
 
     public static CharSequence formatServings(int servings) {
         return String.format("Served: %d", servings);
+    }
+
+    public interface IsPersistable<T> {
+
+        List<T> consumeCusor(Cursor cursor);
+
     }
 }
