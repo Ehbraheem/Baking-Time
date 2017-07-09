aab7bb08f6b3d4bd420e7889275daddafb6cf17e
Ehbraheem <ehbraheem@gmail.com>
2017-07-09 01:22:48 -0700

    Add data query logic

--- app/src/main/java/com/example/profbola/bakingtime/ui/RecipeDetailActivity.java
+++ app/src/main/java/com/example/profbola/bakingtime/ui/RecipeDetailActivity.java
@@ -1,10 +1,17 @@
 package com.example.profbola.bakingtime.ui;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.os.ResultReceiver;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -13,14 +20,20 @@ import com.example.profbola.bakingtime.R;
 import com.example.profbola.bakingtime.models.Ingredient;
 import com.example.profbola.bakingtime.models.Recipe;
 import com.example.profbola.bakingtime.models.Step;
+import com.example.profbola.bakingtime.provider.RecipeContract;
+import com.example.profbola.bakingtime.services.RecipeService;
 import com.example.profbola.bakingtime.utils.RecipePagerAdapter;
+
+import java.util.List;
 
 /**
  * Created by prof.BOLA on 7/1/2017.
  */
 
 public class RecipeDetailActivity extends AppCompatActivity implements
-        IngredientsFragment.OnIngredientSelected, StepsFragment.OnVideoPlayerSelected {
+        IngredientsFragment.OnIngredientSelected,
+        StepsFragment.OnVideoPlayerSelected,
+        LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final String INGREDIENT_TAB = "ingredients";
     private static final String STEP_TAB = "steps";
@@ -37,6 +50,13 @@ public class RecipeDetailActivity extends AppCompatActivity implements
 
     public static final String INGREDIENTS_KEY = "ingredient";
     public static final String STEP_KEY = "step";
+
+    public static final int STEP_LOADER_ID = 223;
+    public static final int INGREDIENT_LOADER_ID = 445;
+
+    private Recipe mRecipe;
+    private List<Ingredient> mIngredients;
+    private List<Step> mSteps;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -57,11 +77,14 @@ public class RecipeDetailActivity extends AppCompatActivity implements
 
         if (savedInstanceState == null) {
             Recipe recipe = (Recipe) getIntent().getExtras().get(MainActivity.RECIPE);
+            // FIXME: 7/8/2017 Register Loaders
+            mRecipe = recipe;
+
             mIngredientsFragment = new IngredientsFragment();
-            mIngredientsFragment.addData(recipe.ingredients);
 
             mStepsFragment = new StepsFragment();
-            mStepsFragment.addData(recipe.steps);
+
+            setUpLoaders();
         }
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -86,6 +109,7 @@ public class RecipeDetailActivity extends AppCompatActivity implements
 
         viewPager.setAdapter(adapter);
     }
+    
 
     @Override
     public void ingredientClicked(Ingredient ingredient) {
@@ -111,5 +135,81 @@ public class RecipeDetailActivity extends AppCompatActivity implements
             intent.putExtra(type, object);
             startActivity(intent);
         }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        int recipeId = mRecipe.id;
+        switch (id) {
+
+            case STEP_LOADER_ID:
+                Uri STEP_URI = RecipeContract.StepEntry.CONTENT_URI(recipeId);
+                String sortOrder = RecipeContract.StepEntry.COLUMN_ID + " ASC";
+
+                return createQueryCursor(STEP_URI, sortOrder);
+
+            case INGREDIENT_LOADER_ID:
+                Uri INGREDIENT_URI = RecipeContract.IngredientEntry.CONTENT_URI(recipeId);
+
+                return createQueryCursor(INGREDIENT_URI, null);
+
+            default:
+                throw new RuntimeException("Loader Not Implemented: " + id);
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+        switch (loader.getId()) {
+            case INGREDIENT_LOADER_ID:
+                mIngredientsFragment = new IngredientsFragment();
+                mIngredients = Ingredient.convertCursor(data);
+                
+                break;
+            
+            case STEP_LOADER_ID:
+                mStepsFragment = new StepsFragment();
+                mSteps = Step.convertCursor(data);
+                
+                break;
+            
+            default:
+                throw new RuntimeException("Loader Not Implemented: " + loader.getId());
+        }
+
+        // FIXME: 7/8/2017 SetUp Fragments
+        if (mSteps != null && mIngredients != null) {
+            deliverDataToFragments(mSteps, mIngredients);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+
+    private CursorLoader createQueryCursor(Uri contentUri, String sortOrder) {
+        return new CursorLoader(this,
+                                contentUri,
+                                null,
+                                null,
+                                null,
+                                sortOrder);
+    }
+
+    private void setUpLoaders() {
+        getSupportLoaderManager().restartLoader(STEP_LOADER_ID, null, this);
+        getSupportLoaderManager().restartLoader(INGREDIENT_LOADER_ID, null, this);
+    }
+
+    private void deliverDataToFragments(List<Step> steps, List<Ingredient> ingredients) {
+
+        mIngredientsFragment = (IngredientsFragment) viewPager.getAdapter().instantiateItem(viewPager, 0);
+        mIngredientsFragment.addData(ingredients.toArray(new Ingredient[ingredients.size()]));
+        mStepsFragment = (StepsFragment) viewPager.getAdapter().instantiateItem(viewPager, 1);
+        mStepsFragment.addData(steps.toArray(new Step[steps.size()]));
+
+        viewPager.getAdapter().notifyDataSetChanged();
     }
 }
